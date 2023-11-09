6279eec16fbda9115a77f740e35f04535688de1f
Srđan Stjepanović <srdjan.stjepanovic01@gmail.com>
2023-11-09 23:54:25 +0100

    fixed bug in passing lambda as callback in PIR_sen

--- sensors/PIR_sen.py
+++ sensors/PIR_sen.py
@@ -6,24 +6,23 @@ class PIR(object):
     def __init__(self, pin):
         self.pin = pin
 
-    def detect_motion(self, func):
+    def detect_motion(self, func, light_on_event):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.IN)
-
         GPIO.add_event_detect(self.pin, GPIO.RISING, callback=func)
 
 
 
 
 def run_dpir_sen(dpir, callback, light_on_event, stop_event):
-    detect_callback = lambda light_on_evet: callback(light_on_event)
+    detect_callback = lambda _: callback(light_on_event)
     dpir.detect_motion(detect_callback)
     while True:
         if stop_event.is_set():
             break
 
 def run_rpir_sen(dpir, callback, name, stop_event):
-    detect_callback = lambda name: callback(name)
+    detect_callback = lambda _: callback(name)
     dpir.detect_motion(detect_callback)
     while True:
         if stop_event.is_set():
