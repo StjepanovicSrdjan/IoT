b9a01e2128895c7e0af5cfed68aa0fb7a2d5d803
Srđan Stjepanović <srdjan.stjepanovic01@gmail.com>
2023-11-09 22:34:26 +0100

    finished code for real uds

--- components/UDS.py
+++ components/UDS.py
@@ -16,3 +16,11 @@ def run_uds(settings, threads, motion_event, stop_event):
         uds_thread.start()
         threads.append(uds_thread)
         print('UDS simulator started')
+    else:
+        from sensors.UDS_sen import UDS, run_uds_sen
+        print('Starting DUS1 sensor')
+        uds = UDS(settings['trig_pin'], settings['echo_pin'])
+        uds_thread = threading.Thread(target=run_uds_sen, args=(uds, uds_callback, stop_event))
+        uds_thread.start()
+        threads.append(uds_thread)
+        print('DUS1 sensor started')
---
+++ sensors/UDS_sen.py
@@ -0,0 +1,54 @@
+import RPi.GPIO as GPIO
+import time
+
+class UDS(object):
+
+    def __init__(self, trig_pin, echo_pin) -> None:
+        self.trig_pin = trig_pin
+        self.echo_pin = echo_pin
+
+    def set_pin(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.trig_pin, GPIO.OUT)
+        GPIO.setup(self.echo_pin, GPIO.IN)
+
+    def get_distance(self):
+        GPIO.output(self.trig_pin, False)
+        time.sleep(0.2)
+        GPIO.output(self.trig_pin, True)
+        time.sleep(0.00001)
+        GPIO.output(self.trig_pin, False)
+        pulse_start_time = time.time()
+        pulse_end_time = time.time()
+
+        max_iter = 100
+
+        iter = 0
+        while GPIO.input(self.echo_pin) == 0:
+            if iter > max_iter:
+                return None
+            pulse_start_time = time.time()
+            iter += 1
+
+        iter = 0
+        while GPIO.input(self.echo_pin) == 1:
+            if iter > max_iter:
+                return None
+            pulse_end_time = time.time()
+            iter += 1
+
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = (pulse_duration * 34300)/2
+        return distance
+    
+
+def run_uds_sen(uds, callback, stop_event):
+    uds.set_pin()
+    while True:
+        distance = uds.get_distance()
+        if distance is not None:
+            callback(distance)
+        time.sleep(1)
+
+        if stop_event.is_set():
+            break
