7e6fe5d3c48fd2123179a5c0aa913743c82c83e7
AlexanderMishutkin <aleksandr@getstellar.ai>
2024-01-24 13:07:21 +0100

    Embedding

--- server/server.py
+++ server/server.py
@@ -21,6 +21,7 @@ mqtt_client = mqtt.Client()
 mqtt_client.connect("localhost", 1883, 60)
 mqtt_client.loop_start()
 
+
 def on_connect(client, userdata, flags, rc):
     client.subscribe("DB")
     client.subscribe("Temperature")
@@ -31,6 +32,7 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("DS")
     client.subscribe("PIR")
     print("Connected to MQTT broker")
+
 
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = lambda client, userdata, msg: save_to_db(json.loads(msg.payload.decode('utf-8')))
@@ -49,47 +51,9 @@ def save_to_db(data):
     write_api.write(bucket=bucket, org=org, record=point)
 
 
-# Route to store dummy data
-@app.route('/store_data', methods=['POST'])
-def store_data():
-    try:
-        data = request.get_json()
-        store_data(data)
-        return jsonify({"status": "success"})
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)})
-
-
-def handle_influx_query(query):
-    try:
-        query_api = influxdb_client.query_api()
-        tables = query_api.query(query, org=org)
-
-        container = []
-        for table in tables:
-            for record in table.records:
-                container.append(record.values)
-
-        return jsonify({"status": "success", "data": container})
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)})
-
-
-@app.route('/simple_query', methods=['GET'])
-def retrieve_simple_data():
-    query = f"""from(bucket: "{bucket}")
-    |> range(start: -10m)
-    |> filter(fn: (r) => r._measurement == "Humidity")"""
-    return handle_influx_query(query)
-
-
-@app.route('/aggregate_query', methods=['GET'])
-def retrieve_aggregate_data():
-    query = f"""from(bucket: "{bucket}")
-    |> range(start: -10m)
-    |> filter(fn: (r) => r._measurement == "Humidity")
-    |> mean()"""
-    return handle_influx_query(query)
+@app.route('/', methods=['GET'])
+def index():
+    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
